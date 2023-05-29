2eeecf5a87c8dad9a260f58c8d90c2a56ecf15a9
DaBroXS <maks70393@gmail.com>
2023-05-29 21:22:25 +0300

    lr

---
+++ lr15/Js — копия.js
@@ -0,0 +1,16 @@
+let x = prompt('vvedit chuslo')
+if(x == 5){
+    console.log("May")
+}
+else if(x == 1){
+    console.log("Sichen");
+}
+else if(x == 2){
+    console.log("Lyutui");
+}
+else if(x == 3){
+    console.log("Berezen");
+}
+else {
+    console.log("tu daun v ded dom");
+}
---
+++ lr15/Js.js
@@ -0,0 +1,20 @@
+for(i = 1; i <= 100; i++){
+if(i % 2 == 0 && i % 3 == 0 && i % 5 == 0 && i % 7 == 0){
+console.log(i);
+}
+}
+// task 2
+let a = 0
+do {
+    a++
+    if(a % 2 == 1){
+        console.log(a + " - не парне число");
+    }
+    else if(a % 2 == 0)
+    console.log(a + " - парне число")
+}while(a < 10)
+// task 3 
+let b = square(50000)
+function square(b)
+return number / number
+
